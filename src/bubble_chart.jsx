6e97bb22a677d6fde434e3837b19b13c570a442a
Siddhartha Konduru <kondurus@usf.edu>
2024-12-05 20:20:00 -0500

    Create a Reusable Chart Component

---
+++ src/bubble_chart.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const BubbleChartComponent = ({ type, data, options }) => {
+  const chartRef = useRef(null);
+  const chartInstanceRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = chartRef.current.getContext('2d');
+
+    if (chartInstanceRef.current) {
+      chartInstanceRef.current.destroy();
+    }
+
+    chartInstanceRef.current = new Chart(ctx, {
+      type,
+      data,
+      options,
+    });
+
+    return () => {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+        chartInstanceRef.current = null;
+      }
+    };
+  }, [type, data, options]);
+
+  return <canvas ref={chartRef}></canvas>;
+};
+
+export default BubbleChartComponent;
